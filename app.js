f07fa8f6c44fe060b6775c46d63d0f80d0d5ad7c
Bumerang <it.bumerang@gmail.com>
2017-07-03 10:21:13 +0300

    Update app.js
    more balls for big world

--- app.js
+++ app.js
@@ -5,16 +5,12 @@ var game = new Phaser.Game(
     {
         preload: preload,
         create: create,
-        update: update,
-        render: render
+        render: render,
+        update: update
     }
 );
 
 var player;
-<<<<<<< HEAD
-// var total = 0;
-var lengthWorld = {x: 1920, y: 1920, padding: 400}
-=======
 var weapon;
 var lengthWorld = {x: 3920, y: 3920, padding: 400}
 >>>>>>> Update app.js
@@ -22,6 +18,14 @@ var lengthWorld = {x: 3920, y: 3920, padding: 400}
 function render() {
     game.debug.text( balls.countDead()   + ' Dead balls', 32, 32);
     game.debug.text( balls.countLiving() + ' Living balls', 32, 62);
+
+<<<<<<< HEAD
+    game.debug.text("Time until event: " + game.time.events.duration.toFixed(0), 32, 32*32);
+    game.debug.text("Next tick: " + game.time.events.next.toFixed(0), 32, 32*4);
+}
+=======
+}
+>>>>>>> Update app.js
 
 <<<<<<< HEAD
     game.debug.text("Time until event: " + game.time.events.duration.toFixed(0), 32, 32*32);
