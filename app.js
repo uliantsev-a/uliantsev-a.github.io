f549350e021dbf578d8c37355dde9b611a99c605
Bumerang <it.bumerang@gmail.com>
2017-07-06 02:21:51 +0300

    merge reconfigure structure files

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+<<<<<<< HEAD
+=======
+function preload() {
+    game.load.image('background','assets/debug-grid-1920x1920.png');
+    game.load.image('player', 'assets/shmup-ship.png');
+    game.load.image('ball', 'assets/bubble.png');
+    game.load.image('bullet', 'assets/bullet.png');
+    game.load.image('vulkaiser', 'assets/vulkaiser_red.png');
+}
+
+var player;
+var lengthWorld = {x: 3920, y: 3920, padding: 400}
+
 var game = new Phaser.Game(
     window.innerWidth, window.innerHeight,
     Phaser.AUTO,
@@ -11,26 +24,10 @@ var game = new Phaser.Game(
 );
 
 var player;
-var weapon;
 var lengthWorld = {x: 3920, y: 3920, padding: 400}
->>>>>>> Update app.js
+
 
 function render() {
     game.debug.text( balls.countDead()   + ' Dead balls', 32, 32);
     game.debug.text( balls.countLiving() + ' Living balls', 32, 62);
-
-<<<<<<< HEAD
-    game.debug.text("Time until event: " + game.time.events.duration.toFixed(0), 32, 32*32);
-    game.debug.text("Next tick: " + game.time.events.next.toFixed(0), 32, 32*4);
 }
-=======
-}
->>>>>>> Update app.js
-
-<<<<<<< HEAD
-    game.debug.text("Time until event: " + game.time.events.duration.toFixed(0), 32, 32*32);
-    game.debug.text("Next tick: " + game.time.events.next.toFixed(0), 32, 32*4);
-}
-=======
-}
->>>>>>> Update app.js
