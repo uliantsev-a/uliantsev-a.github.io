34dd3fced3a2f2d365f3f8d43e6f04ea70c3f61e
Bumerang <it.bumerang@gmail.com>
2017-08-08 06:27:51 +0300

    new multiplayer

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 // Based off of Shawn Van Every's Live Web
 // http://itp.nyu.edu/~sve204/liveweb_fall2013/week3.html
+var express = require('express')
+  , app = express(app)
+  , server = require('http').createServer(app);
 
 var squadrons = [];
 
@@ -14,22 +17,97 @@ function Squadron(id, data){
 
 // Using express: http://expressjs.com/
 var express = require('express');
+var Eureca = require('eureca.io');
+var eurecaServer = new Eureca.Server({allow:['setId', 'spawnEnemy', 'kill', 'updateState']});
+eurecaServer.attach(server);
 // Create the app
-var app = express();
+// serve static files from the current directory
+
+app.use(express.static(__dirname));
+// app.use(express.static('public/game'));
 
 // Set up the server
 // process.env.PORT is related to deploying on heroku
-var server = app.listen(process.env.PORT || 3000, listen);
+// var server = app.listen(process.env.PORT || 3000, listen);
 
-// This call back just tells us that the server has started
-function listen() {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('Example app listening at http://' + host + ':' + port);
+var clients = {};
+
+//detect client connection
+eurecaServer.onConnect(function (conn) {
+    console.log('New Client id=%s ', conn.id, conn.remoteAddress);
+  
+  //the getClient method provide a proxy allowing us to call remote client functions
+    var remote = eurecaServer.getClient(conn.id);
+  
+  //register the client
+  clients[conn.id] = {id:conn.id, remote:remote}
+  
+  //here we call setId (defined in the client side)
+  remote.setId(conn.id);
+});
+
+//detect client disconnection
+eurecaServer.onDisconnect(function (conn) {
+    console.log('Client disconnected ', conn.id);
+  
+  var removeId = clients[conn.id].id;
+  
+  delete clients[conn.id];
+  
+  for (var c in clients)
+  {
+    var remote = clients[c].remote;
+    
+    //here we call kill() method defined in the client side
+    remote.kill(conn.id);
+  } 
+});
+
+eurecaServer.exports.kill = function( id )
+{
+  //var conn = this.connection;
+  remote.kill( id )
 }
 
-app.use(express.static('public'));
 
+eurecaServer.exports.handshake = function()
+{
+  //var conn = this.connection;
+  for (var c in clients)
+  {
+    var remote = clients[c].remote;
+    for (var cc in clients)
+    {
+      //send latest known position
+      var x = clients[cc].laststate ? clients[cc].laststate.x:  0;
+      var y = clients[cc].laststate ? clients[cc].laststate.y:  0;
+
+      remote.spawnEnemy(clients[cc].id, x, y );   
+    }
+  }
+}
+
+eurecaServer.exports.handleKeys = function (keys) {
+  var conn = this.connection;
+  var updatedClient = clients[conn.id];
+  
+  for (var c in clients)
+  {
+    var remote = clients[c].remote;
+    remote.updateState(updatedClient.id, keys);
+    
+    //keep last known state so we can send it to new connected clients
+    clients[c].laststate = keys;
+  }
+}
+
+// This call back just tells us that the server has started
+// function listen() {
+//   var host = server.address().address;
+//   // var host = '255.255.255.255'
+//   var port = server.address().port;
+//   console.log('Example app listening at http://' + host + ':' + port);
+// }
 
 // WebSocket Portion
 // WebSockets work with the HTTP server
@@ -42,21 +120,8 @@ io.sockets.on('connection',
   function (socket) {
   
     console.log("We have a new client: " + socket.id);    
-  
-    // When this user emits, client side: socket.emit('otherevent',some data);
-    // setInterval(heartbeat, 100);
-    // function heartbeat(){
-    //   io.sockets.emit('heartbeat', squadrons);
-    // }
-
     socket.on('start',
       function(data) {
-        // Data comes in as whatever was sent, including objects        
-        // for (var i = 0; i < data.length; i++){
-        //   // console.log(socket.id + "| " + data[i].x + ", " + data[i].y + '| number Ship: ' + i);          
-        // }        
-        io.sockets.emit('heartbeat', squadrons);
-
 
         var squadron;
         for (var q=0; q < squadrons.length; q++){
@@ -74,7 +139,7 @@ io.sockets.on('connection',
           squadron.coordShips = data.coordShips;
           squadron.inertia = data.inertia;
         }
-        // console.info([data.inertia,data.coordShips])
+        io.sockets.emit('heartbeat', squadrons);
       }
     );
     
@@ -88,3 +153,5 @@ io.sockets.on('connection',
     });
   }
 );
+
+server.listen(3000);
